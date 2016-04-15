8151b8bf0dbfa2a80bcddcfb57d6fbed88116c5c
1BoydBen <1BoydBen@rbh20077.ihdsb.CA>
2016-04-15 08:37:53 -0400

    Added main class (initial commit)

---
+++ Graphing Calculator/src/ScientificCalculations.java
@@ -0,0 +1,127 @@
+
+public class ScientificCalculations
+{
+	String operationType;
+	double variable1;
+	double varieable2;
+
+	public static void main(String[] args)
+	{
+
+	}
+
+	// first variable
+	// operation
+	// second variable
+	// answer
+	// answer become first vareiable and repeat
+
+	public double sin()
+	{
+		return 0;
+
+	}
+
+	public double cos()
+	{
+		return 0;
+
+	}
+
+	public double tan()
+	{
+		return 0;
+
+	}
+
+	public double ln()
+	{
+		return 0;
+
+	}
+
+	public double log()
+	{
+		return 0;
+
+	}
+
+	public double factorial()
+	{
+		return 0;
+
+	}
+
+	public double power()
+	{
+		return 0;
+
+	}
+
+	public double sqaureRoot()
+	{
+		return 0;
+
+	}
+
+	public double add()
+	{
+		return 0;
+
+	}
+
+	public double subtract()
+	{
+		return 0;
+
+	}
+
+	public double divide()
+	{
+		return 0;
+
+	}
+
+	public double multiply()
+	{
+		return 0;
+
+	}
+
+	public double inverseSin()
+	{
+		return 0;
+
+	}
+
+	public double inverseCos()
+	{
+		return 0;
+
+	}
+
+	public double inverseTan()
+	{
+		return 0;
+
+	}
+
+	public double tenToTheX()
+	{
+		return 0;
+
+	}
+
+	public double eToTheX()
+	{
+		return 0;
+
+	}
+
+	public double sqaured()
+	{
+		return 0;
+
+	}
+
+}
